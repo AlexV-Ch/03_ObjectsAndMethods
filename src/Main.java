332431e8e5e1ef766d92d925a44d68faa150e6f3
auto_1 <alexch@yandex.ru>
2025-01-10 15:18:40 +0300

    Выполнено static 2 предварительно 04

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk ");
+        basket.add("Сыр", 140);
+        basket.print("Сыр ");
         Basket basket_1 = new Basket();
         basket_1.add("Масло", 80, 2, 10);
         basket_1.print("Масло ");
         Basket basket_2 = new Basket();
         basket_2.add("Молоко", 70, 5);
-        basket_2.print("Молоко");
+        basket_2.add("Хлеб", 30, 3);
+        basket_2.print("Молоко ");
 
         System.out.println("Средняя стоимость одного товара " + Basket.getAveragePriceProduct() + " руб.");
         System.out.println("Средняя стоимость корзины " + Basket.getAverageCostBasket() + " руб.");
